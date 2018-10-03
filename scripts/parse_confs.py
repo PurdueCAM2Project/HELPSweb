c7fc8bfe59659d9f2cbe5bc92af11c1ff3a273f4
nhendy <nhendy@purdue.edu>
2018-10-03 15:40:09 -0400

    json files

---
+++ scripts/parse_confs.py
@@ -0,0 +1,28 @@
+import re
+from pprint import pprint as pp
+import json
+from codecs import decode
+
+
+pattern = r"^(?P<authors>[^\"]*)\"(?P<paper_name>.*)[\"'], (?P<conf>.*)"
+with open('conf.txt', 'r', encoding='utf-8') as file:
+    data = file.read()
+
+matches = re.finditer(pattern, data, re.MULTILINE | re.ASCII)
+pp([m.group(0) for m in matches])
+result = []
+
+for match in matches:
+    authors = str(match['authors'].strip())
+    paper_name = str(match['paper_name'].strip())
+    conference = str(match['conf'].strip())
+    paper_dict = {'authors': authors, 'paper_name': paper_name, 'conference': conference}
+    result.append(paper_dict)
+
+
+with open('conf.json', 'w') as file:
+    json.dump(result, file, indent=4)
+
+# pp(result)
+
+print(len(result))
